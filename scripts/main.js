86545fca83d15a014f02c70da63008ec95b4b318
gayatrib <gayatri.sriram@gmail.com>
2017-07-31 07:00:25 +0530

    i

--- scripts/main.js
+++ scripts/main.js
@@ -103,8 +103,8 @@ var yAxis = d3.svg.axis()
     .enter().append("circle")
       .attr("class", "dot")
       .attr("r", 3.5)
-	  .attr("cx",function(d) { return x_new(d.AvgGDP);  })      
-      .attr("cy", function(d) { return y_new(d.AvgArableLand); })
+	  .attr("cx",function(d) { return x(d.AvgGDP);  })      
+      .attr("cy", function(d) { return y(d.AvgArableLand); })
       .style("fill", function(d) { return color(d.continent); });
 
   var legend = svg.selectAll(".legend")
