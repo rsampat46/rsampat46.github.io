71863ecc3c3dce745fe811f5bca9ab126dd2f854
gayatrib <gayatri.sriram@gmail.com>
2017-08-01 10:49:04 +0530

    a

--- scripts/main.js
+++ scripts/main.js
@@ -130,22 +130,7 @@ var tooltip = d3.select("body").append("div")
 	  .on("mouseover", tipMouseover)
       .on("mouseout", tipMouseout);
 
-  var legend = svg.selectAll(".legend")
-      .data(color.domain())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-      legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
-      legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { if(color === "#2CA02C"){ return "South Africa";} else{return d["continent"]; }});
+  
 
 });
   
